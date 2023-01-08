1a0d1cd01aa433ea23071f7cbd9aa0ce9d415557
Nechai_Ihor <fredduke1983@gmail.com>
2023-01-09 01:14:40 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsSelector = document.querySelector("#ingredients");
+
+const elementsLiArray = ingredients.map((ingredient) => {
+  const elementsLi = document.createElement("li");
+  elementsLi.classList.add("item");
+  elementsLi.textContent = ingredient;
+  return elementsLi;
+});
+
+ingredientsSelector.append(...elementsLiArray);
