e7823b06607ff654d11c7cca1f00ef2239c4ab02
Nechai_Ihor <fredduke1983@gmail.com>
2023-01-11 21:21:02 +0200

    start task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,26 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputNumEl = document.querySelector('#controls input');
+const boxesEl = document.querySelector('#boxes');
+const buttons = document.querySelectorAll('button');
+const btnCreate = buttons[0];
+const btnDestroy = buttons[1];
+const elemsDiv = [];
+
+inputNumEl.addEventListener('change', e => {
+  console.log(inputNumEl.value);
+});
+
+btnCreate.addEventListener('click', e => {
+  const createDivEl = document.createElement('div');
+  for (let i = 0; i <= inputNumEl.value; i += 1) {
+    elemsDiv.push(createDivEl);
+  }
+  const elements = elemsDiv.map(a => a);
+  boxesEl.insertAdjacentElement('beforeend', ...elements);
+});
+btnDestroy.addEventListener('click', e => {
+  console.log('destroy');
+});
