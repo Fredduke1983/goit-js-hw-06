6c8606fcb316033292d4fa32c49e24ae90b036ae
Nechai_Ihor <fredduke1983@gmail.com>
2023-01-11 20:17:53 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const hexColorEl = document.querySelector('.color');
+const btnChngColor = document.querySelector('.change-color');
+
+btnChngColor.addEventListener('click', e => {
+  const color = getRandomHexColor();
+  hexColorEl.textContent = color;
+  document.body.style.backgroundColor = color;
+});
