4fc46a62a4fa615959a946d2e0080c626935c9f5
Nechai_Ihor <fredduke1983@gmail.com>
2023-01-11 20:07:37 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginFormEl = document.querySelector('.login-form');
+const dataForm = {};
 
+loginFormEl.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (
+    loginFormEl.elements[0].value == '' ||
+    loginFormEl.elements[1].value == ''
+  ) {
+    alert('Всі поля повинні бути заповнені');
+  } else {
+    dataForm.email = loginFormEl.elements[0].value;
+    dataForm.password = loginFormEl.elements[1].value;
+    console.log(`Email: ${dataForm.email} - Password: ${dataForm.password}`);
+    loginFormEl.reset();
+  }
+});
