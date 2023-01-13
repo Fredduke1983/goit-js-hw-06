82c5707396d0b3874d5fb5ba6f2ae5d04bde07e6
Nechai_Ihor <fredduke1983@gmail.com>
2023-01-13 12:50:02 +0200

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -4,38 +4,46 @@ function getRandomHexColor() {
 
 const inputNumEl = document.querySelector('#controls input');
 const boxesEl = document.querySelector('#boxes');
-// const divsElBoxes = boxesEl.querySelector('div');
 const btnCreate = document.querySelector('[data-create]');
 const btnDestroy = document.querySelector('[data-destroy]');
-const elemsDiv = [];
-
-inputNumEl.addEventListener('change', e => {
-  // console.log(inputNumEl.value);
-});
+let width = 30;
+let height = 30;
 
 btnCreate.addEventListener('click', e => {
   createBoxes(inputNumEl.value);
-  // for (let i = 0; i < inputNumEl.value; i++) {
-  //   const createDivEl = document.createElement('div');
-  //   elemsDiv.push(createDivEl);
-  // }
-  // boxesEl.append(...elemsDiv.map(a => a));
 });
 
 btnDestroy.addEventListener('click', e => {
-  for (let i = 0; i < inputNumEl.value; i++) {
-    const divsElBoxes = boxesEl.querySelector('div');
-    divsElBoxes.remove();
-  }
-
-  console.log('destroy');
+  // for (let i = 0; i < inputNumEl.value; i++) {
+  //   const divsElBoxes = boxesEl.querySelector('div');
+  //   divsElBoxes.remove();
+  // }
+  const divsElBoxes = boxesEl.querySelectorAll('div');
+  divsElBoxes.forEach(a => a.remove());
+  width = 30;
+  height = 30;
 });
 
 const createBoxes = amount => {
+  const elemsDiv = [];
+
   for (let i = 0; i < amount; i++) {
     const createDivEl = document.createElement('div');
     elemsDiv.push(createDivEl);
   }
-  boxesEl.append(...elemsDiv.map(a => a));
-  // return;
+
+  boxesEl.append(
+    ...elemsDiv.map(a => {
+      a.style.width = `${width}px`;
+      a.style.height = `${height}px`;
+      a.style.backgroundColor = `${getRandomHexColor()}`;
+      a.style.border = '2px solid black';
+      width += 10;
+      height += 10;
+      return a;
+    })
+  );
 };
+boxesEl.style.display = 'flex';
+boxesEl.style.flexWrap = 'wrap';
+boxesEl.style.gap = '5px';
